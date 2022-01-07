9a7d7001c7e990b936a6037d78f885b4771ba67d
Ildefonso González Sánchez <isanchez.extern@univention.de>
2022-01-07 17:15:58 +0100

    Bug #20933: update the test case get_int_string

--- base/univention-config-registry/tests/test_backend.py
+++ base/univention-config-registry/tests/test_backend.py
@@ -125,8 +125,8 @@ class TestConfigRegistry(object):
 
 	def test_get_int_string(self, ucr0):
 		"""Test set ucr.get_int(key, default)."""
-		ucr0['foo'] = "string"
-		assert ucr0.get_int('foo', "string2") == "string2"
+		ucr0['foo'] = 9 
+		assert ucr0.get_int('foo', 9 ) == 9
 
 	def test_empty_get(self, ucr0):
 		"""Test empty ucr.get(key)."""
