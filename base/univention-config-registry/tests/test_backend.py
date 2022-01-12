0be0b37cfa943153849494cf6178beb851447dbd
Ildefonso González Sánchez <isanchez.extern@univention.de>
2022-01-12 16:02:59 +0100

    Bug #20933: update data type of test for get_int_string

--- base/univention-config-registry/tests/test_backend.py
+++ base/univention-config-registry/tests/test_backend.py
@@ -125,7 +125,7 @@ class TestConfigRegistry(object):
 
 	def test_get_int_string(self, ucr0):
 		"""Test set ucr.get_int(key, default)."""
-		ucr0['foo'] = 9 
+		ucr0['foo'] = "9" 
 		assert ucr0.get_int('foo', 9) == 9
 
 	def test_empty_get(self, ucr0):
