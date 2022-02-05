610f7017bfc182c3176ff736e645d3d28facc96e
Julia Bremer <bremer@univention.de>
2022-02-05 21:02:56 +0100

    Bug #54438: New UDM Rest call "service-specific-password"
    This call takes a "service" argument and generates a password which
    is then set by UDM in the related LDAP attribute

--- management/univention-directory-manager-rest/src/univention/admin/rest/module.py
+++ management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -92,6 +92,8 @@ import univention.admin.syntax as udm_syntax
 import univention.admin.types as udm_types
 from univention.config_registry import handler_set
 from univention.admin.rest.shared_memory import shared_memory, JsonEncoder
+from univention.password import password_config, generate_password
+
 
 import univention.udm
 
@@ -3694,6 +3696,39 @@ univentionObjectType: settings/license
 		self.content_negotiation({'message': _('The license was imported successfully.')})
 
 
+class ServiceSpecificPassword(Resource):
+
+	@sanitize_body_arguments(
+		service=StringSanitizer(required=True),
+	)
+	@tornado.gen.coroutine
+	def post(self, dn):
+		object_type = 'users/user'
+		module = get_module(object_type, dn, self.ldap_connection)
+		if module is None:
+			raise NotFound(object_type, dn)
+
+		service_type = self.get_body_argument('service')
+
+		try:
+			cfg = password_config(service_type)
+			new_password = generate_password(**cfg)
+		except Exception as exc:
+			raise HTTPError(400, _('Generation of the service specific password failed. Error: %s') % (exc,))
+
+		obj = yield self.pool.submit(module.get, dn)
+		obj['serviceSpecificPassword'] = {'service': service_type, 'password': new_password}
+
+		try:
+			yield self.pool.submit(obj.modify)
+		except udm_errors.unsupportedService as exc:
+			raise HTTPError(400, _('Service %s does not support service specific passwords. Error: %s') % (service_type, exc,))
+		except Exception as exc:
+			raise HTTPError(400, _('Failed to set service specific password. Error: %s') % (exc,))
+		result = {'service': service_type, 'password': new_password}
+		self.content_negotiation(result)
+
+
 def decode_properties(module, obj, properties):
 	for key, value in properties.items():
 		prop = module.get_property(key)
@@ -3807,6 +3842,7 @@ class Application(tornado.web.Application):
 			(r"/udm/%s/properties/%s/default" % (object_type, property_), DefaultValue),
 			(r"/udm/networks/network/%s/next-free-ip-address" % (dn,), NextFreeIpAddress),
 			(r"/udm/progress/([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})", Operations),
+			(r"/udm/users/user/%s/service-specific-password" % (dn,), ServiceSpecificPassword),
 			# TODO: decorator for dn argument, which makes sure no invalid dn syntax is used
 		], default_handler_class=Nothing, **kwargs)
 
