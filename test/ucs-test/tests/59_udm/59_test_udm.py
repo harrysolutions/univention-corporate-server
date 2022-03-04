eec7a4916d676a1426817d2019223506c1164dd0
Johannes Lohmer <lohmer@univention.de>
2022-03-04 18:01:45 +0100

    fixup! Bug #53538: [ucs-test] 59_udm merged files

--- test/ucs-test/tests/59_udm/59_test_udm.py
+++ test/ucs-test/tests/59_udm/59_test_udm.py
@@ -25,6 +25,7 @@ MODULE = 'tests/ipservice'
 @pytest.mark.exposure('careful')
 def test_multi_valued_rdn(udm):
 	"""Create UDM object with multi-malued RDN"""
+	# bugs: [#40129]
 	print("Creating...")
 	testing = udm.create_object('container/cn', name=random_name())
 	str_tcp = udm.create_object(MODULE, position=testing, name='echo', protocol='tcp', port='7')
@@ -68,12 +69,14 @@ def test_multi_valued_rdn(udm):
 	utils.verify_ldap_object(new_tcp)
 
 
-@pytest.mark.tags('udm', 'apptest', 'SKIP')
+@pytest.mark.skip()
+@pytest.mark.tags('udm', 'apptest')
 @pytest.mark.roles('domaincontroller_master')
 @pytest.mark.exposure('dangerous')
 @pytest.mark.parametrize('object_type', ['container/cn', 'mail/domain'])
 def test_policy_reference_after_object_change(udm, object_type):
 	"""policy reference works after changing the objects dn"""
+	# bugs: [41694]
 	print('testing', object_type)
 	policy = udm.create_object('policies/pwhistory', **{'name': random_string()})
 	old_dn = udm.create_object(object_type, **{'name': random_string()})
@@ -90,6 +93,7 @@ def test_policy_reference_after_object_change(udm, object_type):
 @pytest.mark.exposure('careful')
 def test_create_existing_object_does_not_policy_reference(udm):
 	"""Test that --policy-reference is not evaluated when creating a existing object"""
+	# bugs:  [38856]
 	dhcp_service = udm.create_object('dhcp/service', service=random_name())
 	policy = udm.create_object('policies/pwhistory', **{'name': random_string()})
 
@@ -107,17 +111,19 @@ def test_create_existing_object_does_not_policy_reference(udm):
 @pytest.mark.exposure('careful')
 def test_remove_values_from_ucr_policy(udm):
 	"""Test removing values from UCR policy"""
+	# bugs: [43562]
 	policy = udm.create_object('policies/registry', name='test', registry=['foo bar', 'bar baz'])
 	utils.verify_ldap_object(policy, {'univentionRegistry;entry-hex-666f6f': ['bar'], 'univentionRegistry;entry-hex-626172': ['baz']})
 	udm.modify_object('policies/registry', dn=policy, remove={'registry': ['bar baz']})
 	utils.verify_ldap_object(policy, {'univentionRegistry;entry-hex-666f6f': ['bar']})
 
 
-@pytest.mark.tags('udm')
+@pytest.mark.tags('apptest')
 @pytest.mark.roles('domaincontroller_master')
 @pytest.mark.exposure('careful')
 def test_remove_values_from_share_sambaCustomSettings(udm):
 	"""Test removing values from share"""
+	# bug: [41072]
 	share = udm.create_object('shares/share', name='test', host='localhost', path='/path/', sambaCustomSettings='"follow symlinks" "yes"')
 	utils.verify_ldap_object(share, {'univentionShareSambaCustomSetting': ['follow symlinks = yes']})
 	udm.modify_object('shares/share', dn=share, remove={'sambaCustomSettings': ['"follow symlinks" "yes"']})
